e1fdc0685550a86971a3286a2d5942d3f752e9aa
HS-hyeokjin <youngurwls@gmail.com>
2023-02-23 23:49:51 +0900

    java

--- Java_PR/src/com/company/design/programers/Solution.java
+++ Java_PR/src/com/company/design/programers/Solution.java
@@ -2,6 +2,7 @@ package com.company.design.programers;
 
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution {
     //평균값
@@ -149,9 +150,154 @@ class Solution {
             }
         }
         return answer;
+    }
 
+    //순서쌍의 개수
+    public int solution14(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) answer++;
+        }
+        return answer;
+    }
 
+    //가위 바위 보
+    public String solution15(String rsp) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < rsp.length(); i++) {
+            answer.append(rsp.split("")[i].equals("2") ? "0"
+                    : rsp.split("")[i].equals("0") ? "5" : "2");
+        }
+        return answer.toString();
+    }
+
+    //2차원으로 만들기
+    public int[][] solution16(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        for (int i = 0; i < num_list.length / n; i++) {
+            int[] arr = new int[n];
+
+            for (int j = 0; j < n; j++) {
+                arr[j] = num_list[i * n + j];
+            }
+            answer[i] = arr;
+        }
+        return answer;
+    }
+
+    //공 던지기
+    public int solution17(int[] numbers, int k) {
+        int answer = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (numbers.length <= i * 2) {
+                answer = numbers[(i * 2) % numbers.length];
+            } else
+                answer = numbers[i * 2];
+
+        }
+        return answer;
+    }
+
+    //최대값 만들기(1)
+    public int solution18(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 2] * numbers[numbers.length - 1];
+    }
+
+    //모음 제거 --
+    public String solution19(String my_string) {
+        String answer = "";
+        String[] vowel = {"a", "e", "i", "o", "u"};
+        for (int i = 0; i < vowel.length; i++) {
+            if (my_string.contains(vowel[i])) {
+                answer = my_string.replaceAll(vowel[i], "");
+                my_string = answer;
+            } else {
+                answer = my_string;
+            }
+        }
+        return answer;
+    }
+
+    //문자열 정렬하기 (1) --
+    public int[] solution20(String my_string) {
+        return Arrays.stream(my_string.split(""))
+                .filter(i -> i.matches("[0-9]"))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+    }
+
+    //숨어있는 숫자의 덧셈 (1)
+    public int solution21(String my_string) {
+        int answer = 0;
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) < 57)
+                answer += (int) my_string.charAt(i) - 48;
+        }
+
+        return answer;
+    }
+
+    //컨트롤 제트
+    public int solution22(String s) {
+        int answer = 0;
+        String[] stringArray = s.split(" ");
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].equals("Z")) {
+                answer -= Integer.parseInt(stringArray[i - 1]);
+                continue;
+            }
+            answer += Integer.parseInt(stringArray[i]);
+        }
+        return answer;
+    }
+
+    //캐릭터의 좌표
+    public int[] solution23(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        for (int i = 0; i < keyinput.length; i++) {
+            switch (keyinput[i]) {
+                case "up": {
+                    if (answer[1] >= board[1] / 2) {
+                        continue;
+                    }
+                    answer[1] += 1;
+                }
+                case "down": {
+                    if (answer[1] <= -(board[1] / 2)) {
+                        continue;
+                    }
+                    answer[1] -= 1;
+                }
+                case "right": {
+                    if (answer[0] >= board[0] / 2) {
+                        continue;
+                    }
+                    answer[0] += 1;
+                }
+                case "left": {
+                    if (answer[0] <= -(board[0] / 2)) {
+                        continue;
+                    }
+                    answer[0] -= 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    //숨어있는 숫자의 덧셈 (2)
+    public int solution24(String my_string) {
+        int answer = 0;
+        String[] array = my_string.split("[a-zA-Z]");
+        for (int i = 0; i < array.length; i++) {
+            answer += !array[i].isEmpty() ? Integer.parseInt(array[i]) : 0;
+        }
+        return answer;
     }
 
 
 }
+
